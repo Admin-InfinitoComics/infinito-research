a37ed44914ec89b1249a89e60953aaa5c1ac21c8
Admin-InfinitoComics <admin.it@infinitocomics.com>
2025-09-27 20:55:05 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const FRONTEND_BASE_URL= import.meta.env.FRONTEND_URL || "https://infinitocomics.com";
-export const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:4500";
+export const BACKEND_URL = import.meta.env.BACKEND_URL || "https://api.infinitocomics.com";
